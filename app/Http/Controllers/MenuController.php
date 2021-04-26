fd1402e04391139529be22ed03f9320e9bbe1901
Pankaj Kumar <pankaj.kumar@signure.com>
2021-04-26 22:02:00 +0530

    Added getMenuItems

--- app/Http/Controllers/MenuController.php
+++ app/Http/Controllers/MenuController.php
@@ -4,11 +4,31 @@
 namespace App\Http\Controllers;
 
 use App\Models\MenuItem;
+use App\Services\MenuService;
 use Illuminate\Routing\Controller as BaseController;
 
 class MenuController extends BaseController
 {
+    /**
+     * Assign event service object
+     *
+     * @var object
+     **/
+    protected $service;
+    /**
+     * Create a new login service instance
+     *
+     * @param MenuService object $eventService login service object
+     *
+     * @return void
+     **/
+    public function __construct(MenuService $menuService)
+    {
+        $this->service = $menuService;
+    }
+
     public function getMenuItems() {
-        throw new \Exception('not implemented');
+        $menus = $this->service->menuList();
+        return $menus;
     }
 }
--- app/Models/MenuItem.php
+++ app/Models/MenuItem.php
@@ -9,4 +9,18 @@ use Illuminate\Database\Eloquent\Model;
 class MenuItem extends Model
 {
 
+    /**
+     * Relation with Workshop
+     *
+     * @return object
+     **/
+    public function children() {
+        return $this->hasMany('App\Models\MenuItem','parent_id');
+    }
+
+    static function search(){
+        $events = MenuItem::with('children')->has('children')->get();
+        return $events;
+    }
+
 }
